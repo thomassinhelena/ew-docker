b1b504a9e19d8191c0c8aff372641f157455c3b1
Hélèna Thomassin <thomassin.helena@gmail.com>
2021-01-05 16:12:44 +0100

    Update

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ process.on("SIGTERM", () => {
 
 const init = async () => {
   const app = express();
-  console.log("Connecting to MongoDB database");
+  console.log("Connecting to MongoDB");
   await mongoose.connect("mongodb://mongodb:27017/ew-docker", {
     useNewUrlParser: true,
     useUnifiedTopology: true,
